ad60d4518ecd520d01e1f5a7fc8fbcdad8441468
Saruar <saruar.ryskaliyev@nu.edu.kz>
2024-04-08 00:05:44 +0500

    chrome picker

--- src/App.js
+++ src/App.js
@@ -1,35 +1,19 @@
 import { useState, useCallback } from 'react';
-import { SketchPicker, HuePicker, AlphaPicker } from 'react-color';
+import { ChromePicker} from 'react-color';
 import _ from 'lodash';
 
 function App() {
   const [currentColor, setCurrentColor] = useState({
     r: 255,
     g: 255,
-    b: 102,
+    b: 255,
     a: 1
   });
-
 
   const handleColorChange = useCallback(_.throttle((color) => {
     setCurrentColor(color.rgb);
   }, 100), []); 
 
-  const handleHueChange = useCallback(_.throttle((color) => {
-    setCurrentColor(current => ({
-      ...current,
-      r: color.rgb.r,
-      g: color.rgb.g,
-      b: color.rgb.b
-    }));
-  }, 100), []);
-
-  const handleAlphaChange = useCallback(_.throttle((color) => {
-    setCurrentColor(current => ({
-      ...current,
-      a: color.rgb.a
-    }));
-  }, 100), []);
 
   const appStyle = {
     backgroundColor: `rgba(${currentColor.r}, ${currentColor.g}, ${currentColor.b}, ${currentColor.a})`,
@@ -40,34 +24,15 @@ function App() {
     textAlign: "center",
   };
 
-  const pickerContainerStyle = {
-    marginTop: '20px',
-  };
-
   return (
     <div className="App" style={appStyle}>
       <div>
         <h1>Color Palette</h1>
-        <SketchPicker 
+        <ChromePicker 
           color={currentColor}
           onChange={handleColorChange}
         />
-        <div style={pickerContainerStyle}>
-          <h2>Hue Picker</h2>
-          <HuePicker
-            color={currentColor}
-            onChange={handleHueChange}
-            width="90%"
-          />
-        </div>
-        <div style={pickerContainerStyle}>
-          <h2>Alpha Picker</h2>
-          <AlphaPicker
-            color={currentColor}
-            onChange={handleAlphaChange}
-            width="90%"
-          />
-        </div>
+    
       </div>
     </div>
   );
