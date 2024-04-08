cf3f1e1cf461b7779014eead86ca032900c67179
Saruar <saruar.ryskaliyev@nu.edu.kz>
2024-04-08 22:40:04 +0500

    fix  edit color

--- src/pages/EditMyColors.js
+++ src/pages/EditMyColors.js
@@ -1,51 +1,49 @@
-import React, { useState, useEffect, useCallback } from "react";
-import { ChromePicker } from 'react-color';
+import React, { useState, useEffect } from "react";
+import { PhotoshopPicker } from 'react-color';
 import { ReactComponent as IconClose } from "./icon-close.svg";
 import "./EditMyColors.css";
 
 export const EditMyColors = ({ isOpen, onClose, initialColor, onColorUpdate }) => {
   const [currentColor, setCurrentColor] = useState(initialColor);
+  const [key, setKey] = useState(Date.now()); 
 
   useEffect(() => {
-    setCurrentColor(initialColor); // Ensure that initialColor changes are reflected
-  }, [initialColor]);
+    setCurrentColor(initialColor);
+    setKey(Date.now()); 
+  }, [initialColor, isOpen]); 
 
-  const handleColorChange = useCallback((color) => {
-    setCurrentColor(color.hex);  // Update the current color directly
-    if (onColorUpdate) {
-      onColorUpdate(color.hex);  // Call the callback if provided
-    }
-
-
-  }, [onColorUpdate]);
-
-  const handleCancel = () => {
-    if (onColorUpdate) {
-      onColorUpdate(initialColor); // Revert to the initial color
-    }
-    onClose(); // Close the modal
+  const handleColorChange = (color) => {
+    setCurrentColor(color.hex);
   };
 
-  const onWrapperClick = (event) => {
-    if (event.target.classList.contains("modal-wrapper")) onClose();
-  }
+  const handleSave = () => {
+    if (onColorUpdate) {
+      onColorUpdate(currentColor); 
+    }
+    onClose(); 
+  };
+
+  const handleCancel = () => {
+    onClose(); 
+  };
 
   if (!isOpen) return null;
 
   return (
-    <div className="modal-wrapper" onClick={onWrapperClick}>
+    <div className="modal-wrapper" onClick={(event) => {
+      if (event.target.classList.contains("modal-wrapper")) onClose();
+    }}>
       <div className="modal-content" style={{ textAlign: 'center' }}>
         <button className="modal-close-button" onClick={onClose}>
           <IconClose />
         </button>
-        <ChromePicker
+        <PhotoshopPicker
+          key={key} 
           color={currentColor}
           onChangeComplete={handleColorChange}
+          onAccept={handleSave}
+          onCancel={handleCancel}
         />
-        <div>
-          <button onClick={() => onClose()} style={{ marginRight: '10px' }}>Save</button>
-          <button onClick={handleCancel}>Cancel</button>
-        </div>
       </div>
     </div>
   );
